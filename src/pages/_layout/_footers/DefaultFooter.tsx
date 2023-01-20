c22169d913bcb7f67ca067ebe05503890beee7aa
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2023-01-20 11:56:39 +0300

    cleared extra from main page, preparing to dev edit profile

--- src/pages/_layout/_footers/DefaultFooter.tsx
+++ src/pages/_layout/_footers/DefaultFooter.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import classNames from 'classnames';
-import useDarkMode from '../../../hooks/useDarkMode';
 import Footer from '../../../layout/Footer/Footer';
 
 const DefaultFooter = () => {
-	const { darkModeStatus } = useDarkMode();
 
 	return (
 		<Footer>
 			<div className='container-fluid'>
 				<div className='row'>
 					<div className='col'>
-						<span className='fw-light'>Copyright © 2023 - Version 4.1.5</span>
-					</div>
-					<div className='col-auto'>
-						<a
-							href='/'
-							className={classNames('text-decoration-none', {
-								'link-dark': !darkModeStatus,
-								'link-light': darkModeStatus,
-							})}>
-							<small className='fw-bold'>Facit Theme</small>
-						</a>
+						<span className='fw-light'>Copyright © 2023 - U10 Univercity</span>
 					</div>
 				</div>
 			</div>
--- src/pages/presentation/dashboard/DashboardPage.tsx
+++ src/pages/presentation/dashboard/DashboardPage.tsx
@@ -1,38 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useTour } from '@reactour/tour';
+import React, {useContext, useEffect} from 'react';
+import {useTour} from '@reactour/tour';
 import useDarkMode from '../../../hooks/useDarkMode';
-import { demoPagesMenu } from '../../../menu';
+import {demoPagesMenu} from '../../../menu';
 import PageWrapper from '../../../layout/PageWrapper/PageWrapper';
-import SubHeader, {
-	SubHeaderLeft,
-	SubHeaderRight,
-	SubheaderSeparator,
-} from '../../../layout/SubHeader/SubHeader';
+import SubHeader, {SubHeaderLeft} from '../../../layout/SubHeader/SubHeader';
 import Page from '../../../layout/Page/Page';
-import { TABS, TTabs } from './common/helper';
-import Button, { ButtonGroup } from '../../../components/bootstrap/Button';
-
-import CommonAvatarTeam from '../../../common/other/CommonAvatarTeam';
-
-import CommonDashboardAlert from './common/CommonDashboardAlert';
-import CommonDashboardUserCard from './common/CommonDashboardUserCard';
-import CommonDashboardMarketingTeam from './common/CommonDashboardMarketingTeam';
-import CommonDashboardDesignTeam from './common/CommonDashboardDesignTeam';
-import CommonDashboardIncome from './common/CommonDashboardIncome';
-import CommonDashboardRecentActivities from './common/CommonDashboardRecentActivities';
-import CommonDashboardUserIssue from './common/CommonDashboardUserIssue';
-import CommonDashboardSalesByStore from './common/CommonDashboardSalesByStore';
-import CommonDashboardWaitingAnswer from './common/CommonDashboardWaitingAnswer';
-import CommonMyWallet from '../../_common/CommonMyWallet';
-import CommonDashboardTopSeller from './common/CommonDashboardTopSeller';
 import ThemeContext from '../../../contexts/themeContext';
 
 const DashboardPage = () => {
-	const { mobileDesign } = useContext(ThemeContext);
+	const {mobileDesign} = useContext(ThemeContext);
 	/**
 	 * Tour Start
 	 */
-	const { setIsOpen } = useTour();
+	const {setIsOpen} = useTour();
 	useEffect(() => {
 		if (localStorage.getItem('tourModalStarted') !== 'shown' && !mobileDesign) {
 			setTimeout(() => {
@@ -46,72 +26,18 @@ const DashboardPage = () => {
 
 	const { themeStatus } = useDarkMode();
 
-	const [activeTab, setActiveTab] = useState<TTabs>(TABS.YEARLY);
 
 	return (
 		<PageWrapper title={demoPagesMenu.sales.subMenu.dashboard.text}>
 			<SubHeader>
 				<SubHeaderLeft>
-					<span className='h4 mb-0 fw-bold'>Overview</span>
-					<SubheaderSeparator />
-					<ButtonGroup>
-						{Object.keys(TABS).map((key) => (
-							<Button
-								key={key}
-								color={activeTab === TABS[key] ? 'success' : themeStatus}
-								onClick={() => setActiveTab(TABS[key])}>
-								{TABS[key]}
-							</Button>
-						))}
-					</ButtonGroup>
+					<span className='h4 mb-0 fw-bold'>Profile</span>
 				</SubHeaderLeft>
-				<SubHeaderRight>
-					<CommonAvatarTeam>
-						<strong>Marketing</strong> Team
-					</CommonAvatarTeam>
-				</SubHeaderRight>
 			</SubHeader>
 			<Page container='fluid'>
-				<div className='row'>
-					<div className='col-12'>
-						<CommonDashboardAlert />
-					</div>
-
-					<div className='col-xl-4'>
-						<CommonDashboardUserCard />
-					</div>
-					<div className='col-xl-4'>
-						<CommonDashboardMarketingTeam />
-					</div>
-					<div className='col-xl-4'>
-						<CommonDashboardDesignTeam />
-					</div>
-
-					<div className='col-xxl-6'>
-						<CommonDashboardIncome activeTab={activeTab} />
-					</div>
-					<div className='col-xxl-3'>
-						<CommonDashboardRecentActivities />
-					</div>
-					<div className='col-xxl-3'>
-						<CommonDashboardUserIssue />
-					</div>
-
-					<div className='col-xxl-8'>
-						<CommonDashboardSalesByStore />
-					</div>
-
-					<div className='col-xxl-4 col-xl-6'>
-						<CommonDashboardWaitingAnswer />
-					</div>
-
-					<div className='col-xxl-4 col-xl-6'>
-						<CommonMyWallet />
-					</div>
-					<div className='col-xxl-8'>
-						<CommonDashboardTopSeller />
-					</div>
-				</div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam aut distinctio est facilis
+				illum in, quas quis veritatis! Cumque exercitationem explicabo facilis fugiat, itaque labore laboriosam
+				modi nostrum quia!
 			</Page>
 		</PageWrapper>
 	);
