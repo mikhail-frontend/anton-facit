06236d54f2d167c777a4e638e225cf28bbdc5f38
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2023-01-22 16:39:46 +0300

    ++

--- src/pages/presentation/CourseItem/CourseItem.tsx
+++ src/pages/presentation/CourseItem/CourseItem.tsx
@@ -39,7 +39,7 @@ const CourseItem = () => {
 				<div className='display-4 fw-bold pt-3 pb-5'>{currentCourse ? currentCourse.title : 'Course page'}</div>
 				<div className='row g-4'>
 					<div className='col-12'>
-						<div className={classNames('ratio ratio-21x9', 'rounded-2', 'mb-3')}>
+						<div className={classNames('ratio ratio-21x9', 'rounded-2', 'mb-3', 'bg-l10-danger')}>
 							<img
 								src={currentCourse?.image || ''}
 								alt={currentCourse ? currentCourse.title : 'Course page'}
